e8d010106c77dba446b3b458b33b1cff074e06db
Khushal Jain <jain.khushal1566@gmail.com>
2026-03-28 11:39:22 +0530

    fixed path error

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express    = require('express');
 const http       = require('http');
+const path = require("path");
 const dbPath = path.join("/data", "auction.db");
 const cookieParser = require('cookie-parser');
 const { Server } = require('socket.io');
